76726357dba8895bc031749110937975357d386f
ascorbic1230 <nguyenquoctoan1230@gmail.com>
2022-03-20 21:41:45 +0700

    fix: read/write file data

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -1,9 +1,12 @@
 import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Set;
 
 public class Dictionary {
@@ -19,8 +22,21 @@ public class Dictionary {
         loadData();
     }
 
-    public String findDefinitionBySlangWord(String slangWord) {
+    public String findDefinitionOfSlangWord(String slangWord) {
         return data.get(slangWord);
+    }
+
+    public HashMap<String, String> findSlangWordsByKeyword(String keyword) {
+        HashMap<String, String> slangWords = new HashMap<>();
+
+        Set<String> keySet = data.keySet();
+        for (String slangWord : keySet) {
+            String definition = data.get(slangWord).toLowerCase(Locale.ROOT);
+            if (definition.contains(keyword.toLowerCase(Locale.ROOT)))
+                slangWords.put(slangWord, definition);
+        }
+
+        return slangWords;
     }
 
     public void loadData() {
@@ -37,7 +53,8 @@ public class Dictionary {
             BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(databaseFileName));
 
             int size = data.size();
-            bos.write(size);
+            byte[] bytes = ByteBuffer.allocate(4).putInt(size).array();
+            bos.write(bytes);
 
             Set<String> slangWords = data.keySet();
             for (String slangWord : slangWords) {
@@ -62,8 +79,11 @@ public class Dictionary {
     public void readDatabase() {
         try {
             BufferedInputStream bis = new BufferedInputStream(new FileInputStream(databaseFileName));
+            data.clear();
 
-            int size = bis.read();
+            byte[] sizeBytesArray = new byte[4];
+            bis.read(sizeBytesArray);
+            int size = ByteBuffer.wrap(sizeBytesArray).getInt();
 
             for (int i = 0; i < size; i++) {
                 int slangWordSize = bis.read();
